d3d99f64ced6459b5ddd4260102874768506d2a0
Mahesh Yerudkar <1000037803@hexaware.com>
2023-02-25 02:52:38 +0530

    Second Commit

--- src/main/java/com/mahi/javapassion/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ src/main/java/com/mahi/javapassion/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "currency-exchange", url = "localhost:8000")
+//@FeignClient(value = "currency-exchange", url = "localhost:8000")
+@FeignClient(value = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
